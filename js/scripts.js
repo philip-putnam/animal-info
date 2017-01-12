ffa1944abded7541304b6432e5f0b236c6f996dd
philip putnam <putnamp989@gmail.com>
2017-01-12 15:36:59 -0800

    add img hover cursor pointer in styles.css

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $("#turtleImg").click(function() {
+    $(".col-md-12").hide();
+    $("#turtles").show();
+  });
+
+  $("#snakeImg").click(function() {
+    $(".col-md-12").hide();
+    $("#snakes").show();
+  });
+
+  $("#insectImg").click(function() {
+    $(".col-md-12").hide();
+    $("#insects").show();
+  });
+
+});
